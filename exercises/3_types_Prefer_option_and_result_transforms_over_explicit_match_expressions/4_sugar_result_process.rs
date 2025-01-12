5d16874a4e5e385b4f20669fcb638a819a87263f
sota.n <sota.n.eng@gmail.com>
2025-01-12 10:48:37 +0900

    成功しないように削除すべき行を追加した。

--- exercises/3_types_Prefer_option_and_result_transforms_over_explicit_match_expressions/4_sugar_result_process.rs
+++ exercises/3_types_Prefer_option_and_result_transforms_over_explicit_match_expressions/4_sugar_result_process.rs
@@ -9,8 +9,5 @@ fn divide(a: i32, b: i32) -> Result<i32, &'static str> {
 fn main() {
     let result = divide(10, 3); // TODO: クエスションマークを使ってさらに簡潔に表現しましょう。
 
-    match result {
-        Ok(value) => println!("Result: {}", value),
-        Err(e) => panic!("Result: {}", e),
-    }
+    true // この行は実装時に削除してください。
 }
